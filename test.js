3317645abd8285be83e5f6117d6a29005ea6b078
CasSpicer <114956509+CasSpicer@users.noreply.github.com>
2023-03-14 14:46:00 -0700

    add some code!

---
+++ test.js
@@ -0,0 +1,37 @@
+// -3	great grandfather	great grandmother
+// -2	grandfather	grandmother
+// -1	father	mother
+// 0	me!	me!
+// 1	son	daughter
+// 2	grandson	granddaughter
+// 3	great grandson	great granddaughter
+
+// Try finding your ancestors and offspring with code.
+
+// Create a function that takes a number x and a character y ("m" for male, "f" for female), and returns the name of an ancestor (m/f) or descendant (m/f).
+
+// If the number is negative, return the related ancestor.
+// If positive, return the related descendant.
+// You are generation 0. In the case of 0 (male or female), return "me!".
+// Examples
+// generation(2, "f") ➞ "granddaughter"
+
+// generation(-3, "m") ➞ "great grandfather"
+
+// generation(1, "f") ➞ "daughter"
+
+familyTree = {
+    "-3": { "f": "great grandmother", "m": "great grandfather" },
+    "-2": { "f": "grandmother", "m": "grandfather" },
+    "-1": { "f": "mother", "m": "father" },
+    "0": { "f": "you", "m": "you" },
+    "1": { "f": "daughter", "m": "son" },
+    "2": { "f": "grandaughter", "m": "grandson" },
+    "3": { "f": "great granddaughter", "m": "great grandson" },
+}
+
+function pluckFromFamilyTree(generation, gender) {
+    return familyTree[`${generation}`][gender]
+}
+
+console.log(pluckFromFamilyTree(-3, "f"));
